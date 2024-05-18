fc899a65e43f14c4fc18f3dadc2bf1b3fcedc5ab
Bach Nguyen <nguyenxuanbach03nd@gmail.com>
2024-05-18 17:06:59 +0700

    write aggregation query in mongodb

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -1,12 +1,16 @@
 import Joi from 'joi';
 import { ObjectId } from 'mongodb';
 import { GET_DB } from '~/config/mongodb';
+import { cardModel } from '~/models/cardModel';
+import { columnModel } from '~/models/columnModel';
+import { BOARD_TYPES } from '~/utils/constants';
 
 // Define Collection (Name & Schema)
 const BOARD_COLLECTION_NAME = 'boards';
 const BOARD_COLLECTION_SCHEMA = Joi.object({
   title: Joi.string().required().min(3).max(50).trim().strict(),
   slug: Joi.string().required().min(3).trim().strict(),
+  type: Joi.string().valid(BOARD_TYPES.PUBLIC, BOARD_TYPES.PRIVATE).required(),
   description: Joi.string().required().min(3).max(255).trim().strict(),
   columnOrderIds: Joi.array().items(Joi.string()).default([]),
   createdAt: Joi.date().timestamp('javascript').default(Date.now),
@@ -47,12 +51,39 @@ const findOneById = async (id) => {
 
 const getDetails = async (id) => {
   try {
+    // const result = await GET_DB()
+    //   .collection(BOARD_COLLECTION_NAME)
+    //   .findOne({
+    //     _id: new ObjectId(id),
+    //   });
     const result = await GET_DB()
       .collection(BOARD_COLLECTION_NAME)
-      .findOne({
-        _id: new ObjectId(id),
-      });
-    return result;
+      .aggregate([
+        {
+          $match: {
+            _id: new ObjectId(id),
+            _destroy: false,
+          },
+        },
+        {
+          $lookup: {
+            from: columnModel.COLUMN_COLLECTION_NAME,
+            localField: '_id',
+            foreignField: 'boardId',
+            as: 'columns',
+          },
+        },
+        {
+          $lookup: {
+            from: cardModel.CARD_COLLECTION_NAME,
+            localField: '_id',
+            foreignField: 'boardId',
+            as: 'cards',
+          },
+        },
+      ])
+      .toArray();
+    return result[0] || {};
   } catch (error) {
     throw new Error(error);
   }
@@ -63,5 +94,5 @@ export const boardModel = {
   BOARD_COLLECTION_SCHEMA,
   createNew,
   findOneById,
-  getDetails
+  getDetails,
 };
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,8 @@ export const WHITELIST_DOMAINS = [
   'http://localhost:5173',
   // v.v ví dụ sau này deploy lên domain chính thức
 ];
+
+export const BOARD_TYPES = {
+  PUBLIC: 'public',
+  PRIVATE: 'private',
+};
