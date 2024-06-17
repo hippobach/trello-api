e8f4a449599fab3a3c854f52d7ea94b6b9b3a759
Bach Nguyen <nguyenxuanbach03nd@gmail.com>
2024-06-17 09:28:55 +0700

    update CORS policy not allow localhost

--- src/config/cors.js
+++ src/config/cors.js
@@ -6,9 +6,8 @@ import ApiError from '~/utils/ApiError';
 // Cấu hình CORS Option trong dự án thực tế
 export const corsOptions = {
   origin: function (origin, callback) {
-    // Cho phép việc gọi API bằng POSTMAN trên môi trường dev,
-    // Thông thường khi sử dụng postman thì cái origin sẽ có giá trị là undefined
-    if (!origin && env.BUILD_MODE === 'dev') {
+    // Môi trường là Dev thì cho qua
+    if (env.BUILD_MODE === 'dev') {
       return callback(null, true);
     }
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 // Những domain được phép truy cập tới tài nguyên của server
 export const WHITELIST_DOMAINS = [
-  'http://localhost:5173',
+  // 'http://localhost:5173',
   // v.v ví dụ sau này deploy lên domain chính thức
 ];
 
