c47da1c7fd89d19887f8db415f99a3f6df0aaf22
Bach Nguyen <nguyenxuanbach03nd@gmail.com>
2024-06-21 17:03:43 +0700

    update whitelist domains

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,6 +2,7 @@
 export const WHITELIST_DOMAINS = [
   // 'http://localhost:5173',
   // v.v ví dụ sau này deploy lên domain chính thức
+  'https://trello-web-bay-alpha.vercel.app'
 ];
 
 export const BOARD_TYPES = {
